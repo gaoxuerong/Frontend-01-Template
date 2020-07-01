133c6e36692719b70c20e503e1631f6c7e19d3ec
gaoxuerong <gaoxuerong123@gmail.com>
2020-07-01 22:13:22 +0800

    week12

--- week12/NOTE.md
+++ week12/NOTE.md
@@ -1,1 +1,9 @@
 # 每周总结可以写在这里
+状态机可以写词法分析；
+- 使用LL算法构建AST；
+- 编写带括号的四则运算产生式 ast
+## 字符串算法分析
+字典树 Trie.js
+KMP算法 KMP.js
+括号匹配 Parse.js
+wildCard算法 wildCard算法
---
+++ week12/kmp.js
@@ -0,0 +1,62 @@
+/**
+ * KMP 算法
+ */
+
+const txt = 'I am dsadaabaabaaasda world'
+const pattern = 'aabaabaaa'
+
+function match(pattern, str) {
+	if (!pattern) return true
+	else if (!str) return false
+
+	let nextList = generateNextList(pattern)
+	let j = 0
+
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === pattern[j]) {
+			j++
+		} else {
+			while(j > 0) {
+				j = nextList[j - 1]
+				if (str[i] === pattern[j]) {
+					j++
+					break
+				}
+			}
+		}
+
+		if (j === pattern.length) return true
+		else if (str.length - 1 - i < pattern.length - j) return false
+	}
+
+	return false
+}
+
+function generateNextList(pattern) {
+	let array = [0]
+	let j = 0
+	let isEqual = false
+
+	for (let i = 1; i < pattern.length; i++) {
+		if (pattern[i] === pattern[j]) {
+			j++
+		} else {
+			while(j > 0) {
+				j = array[j - 1]
+				if (pattern[i] === pattern[j]) {
+					isEqual = true
+					break
+				}
+			}
+
+			if (isEqual) {
+				j++
+				isEqual = false
+			}
+		}
+
+		array.push(j)
+	}
+
+	return array
+}
